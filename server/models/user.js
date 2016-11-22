31ed19b90f4ea803d117f1204754d315d7c5c5bc
ryrizo <perrizoryan@gmail.com>
2016-11-21 23:48:51 -0500

    Add JWT schema functions

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,54 @@
-var mongoose = require('mongoose'); //just making models don't need preconfigured mongoose
+const mongoose = require('mongoose'); //just making models don't need preconfigured mongoose
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not an email'
+    }
   },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function() {
+  var user = this;
+  var userObject = user.toObject(); //turn user into regular object
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
