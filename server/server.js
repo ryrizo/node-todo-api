56483d549269d0fc02b7212d0bbd106589bab436
ryrizo <perrizoryan@gmail.com>
2017-01-15 20:37:17 -0500

    Add POST /users/login

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const {ObjectID} = require('mongodb');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose.js'); //destructuring here means only pull the mongoose export from requirement
 var {Todo} = require('./models/todo');
@@ -106,6 +107,19 @@ app.post('/users', (req, res) => {
       res.status(400).send(e);
     });
 });//end post users
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
